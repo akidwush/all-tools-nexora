62db10a6efcc5e5407dd1b93f1c46ae194d958dd
Developer Dika <akidwush@users.noreply.github.com>
2026-09-02 21:50:53 +0700

    fix comic reader sandbox same origin

--- assets/js/features/comic-reader.js
+++ assets/js/features/comic-reader.js
@@ -78,7 +78,7 @@
     body.innerHTML=
       '<div class="nx-comic-embed">'+
         '<div class="nx-comic-loading" id="nxComicLoading">Loading Comic Core</div>'+
-        '<iframe class="nx-comic-frame" id="nxComicFrame" title="All Tools Nexora Baca Komik Full" sandbox="allow-scripts allow-forms allow-popups allow-modals allow-downloads" allow="clipboard-write" referrerpolicy="no-referrer" loading="eager"></iframe>'+
+        '<iframe class="nx-comic-frame" id="nxComicFrame" title="All Tools Nexora Baca Komik Full" sandbox="allow-scripts allow-forms allow-popups allow-modals allow-downloads allow-same-origin" allow="clipboard-write" referrerpolicy="no-referrer" loading="eager"></iframe>'+
       '</div>';
 
     var frame=document.getElementById('nxComicFrame');
